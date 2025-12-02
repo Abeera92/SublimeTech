03f9b26d135d1bff950ae62121b128c26777baac
Dark Pheonix <hashimshub81@gmail.com>
2025-12-02 09:38:03 +0500

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
+// import NotFound from './pages/NotFound'
 import './App.css'
 import Footer from './Component/Footer'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
-function App() {
-
+export default function App() {
   return (
-    <>
-    <Footer/>
-    </>
+    <div className="app">
+      {/* Navigation */}
+      <nav className="navbar">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/about" className="nav-link">About</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+        <Link to="/dashboard" className="nav-link">Dashboard</Link>
+      </nav>
+
+      {/* Main Content */}
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
   )
 }
-
-export default App
---
+++ src/Pages/Contact/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from '../../Component/AboutHero'
+
+export default function Contact() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
